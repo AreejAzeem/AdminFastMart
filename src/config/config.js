ff5d219b4095af5744201908ff9e6058444623fb
AreejAzeem <areejazeem67@gmail.com>
2022-12-26 21:50:56 +0500

    commit for Config

--- src/config/config.js
+++ src/config/config.js
@@ -5,5 +5,5 @@ module.exports = {
  // apiURL: "http://192.168.30.176:4000",
   // apiURL: "http://10.113.49.188:4000",
  // apiURL: "http://localhost:4000",
- apiURL: "http://ec2-3-82-160-223.compute-1.amazonaws.com:4000"
+ apiURL: "http://ec2-3-83-1-211.compute-1.amazonaws.com:4000"
 };
